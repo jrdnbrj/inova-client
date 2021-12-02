b9e3970bd6df311488eb76f01ce59b6a841c8679
Jordan Borja <www.jrdnbrj@gmail.com>
2021-12-01 22:02:31 -0500

    Home 50%

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,21 @@
-import logo2 from '../assets/images/logo2.svg';
+import logo2 from '../assets/images/logo2.svg'
+import building2 from '../assets/images/building2.png'
+import building3 from '../assets/images/building3.png'
+import building4 from '../assets/images/building4.png'
+import kairos from '../assets/images/kairos.svg'
+import logo3 from '../assets/images/logo3.svg'
 
 
 const Home = ({ Navbar }) => {
     return (
-        <div>
-            <div className="home-header">
+        <>
+            <section className="home-header">
                 <Navbar />
                 <div className="row" id="row-correction">
-                    <section className="col">
+                    <div className="col">
                         <img src={logo2} className="logo mobile-hidden" alt="logo" />
-                    </section>
-                    <section className="col">
+                    </div>
+                    <div className="col">
                         <div className="home-contacts">
                             <span id="text-1">OTRA MANERA DE GUIAR</span>
                             <span id="text-2">¿Quiéres un nuevo concepto para tus fotos? Nosotros te damos las herramientas y visión de tu imagen</span>
@@ -22,15 +27,43 @@ const Home = ({ Navbar }) => {
                                 <span>DEPARTAMENTOS</span>
                             </button>
                         </div>
-                    </section>
+                    </div>
                 </div>
                 <section className="socials">
-                    <i class="bi bi-facebook" />
-                    <i class="bi bi-whatsapp" />
-                    <i class="bi bi-instagram" />
+                    <i className="bi bi-facebook" />
+                    <i className="bi bi-whatsapp" />
+                    <i className="bi bi-instagram" />
                 </section>
-            </div>
-        </div>
+            </section>
+            <section className="home-body">
+                <div className="row" id="row-correction">
+                    <div className="col-lg kairos-col">
+                        <div className="kairos-col-item">
+                            <div className="kairos-images">
+                                <img src={building2} className="b-2" alt="building" />
+                                <img src={building3} className="b-3" alt="building" />
+                                <img src={building4} className="b-4" alt="building" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg kairos-col">
+                        <div className="kairos-info">
+                            <img src={kairos} className="kairos" alt="kairos" />
+                            <p>Concebido para la vida ejecutiva de alto rendimiento en la zona de más alta demanda de la ciudad, KAIROS se destaca por su exclusividad, diseño y ubicación estratégica en la ciudad de Quito.</p>
+                            <button id="contactanos">
+                                <span>Contáctanos</span>
+                            </button>
+                            <button id="ver-mas">
+                                <span>Ver más</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>     
+            </section>
+            <section className="lights">
+                <img src={logo3} className="logo" alt="logo" />
+            </section>
+        </>
     )
 }
 
