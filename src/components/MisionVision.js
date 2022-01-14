4f3dfaebfc83c45dd24fa15ab22a5bee2b237f05
Jordan Borja <www.jrdnbrj@gmail.com>
2022-01-14 02:33:28 -0500

    Quienes somos 35%

--- src/components/MisionVision.js
+++ src/components/MisionVision.js
@@ -1,4 +1,4 @@
-const MisionVision = () => {
+const MisionVision = ({ path }) => {
 
     const mision = 'Construimos la mejor alternativa de vivienda en términos de ubicación, calidad y precio, satisfaciendo plenamente a cada uno de nuestros clientes esto se ha logra con el desarrollo integralmente un equipo de trabajo exitoso.'
     const vision = 'Ser la empresa líder de construcción y ventas en la ciudad de Quito, manteniendo un standar de calidad y satisfacción que le brinde a cada cliente trnquilidad en su inversion.'
--- src/pages/QuienesSomos.js
+++ src/pages/QuienesSomos.js
@@ -1,19 +1,63 @@
+import { useEffect } from 'react';
 import MisionVision from '../components/MisionVision';
 
-import logo3 from '../assets/images/logo3.svg'
+// import logo3 from '../assets/images/logo3.svg'
 import somos1 from '../assets/images/somos1.png'
 import somos2 from '../assets/images/somos2.png'
 import somos3 from '../assets/images/somos3.png'
 import somos4 from '../assets/images/somos4.png'
+import quienes from '../assets/images/quienes.jpg'
+import inovaSensaciones from '../assets/videos/inova_sensaciones.mp4'
 
 
 const QuienesSomos = () => {
+
+    useEffect(() => {
+        const video = document.getElementById('inova-sensaciones')
+        video.play()
+    }, [])
+
     return (
         <>
             <section className="us-header" />
-            <MisionVision />
-            <section className="sand">
+            <section className="nuestra-historia">
+                <h2>NUESTRA HISTORIA</h2>
+                <h2>ACERCA DE NOSOTROS</h2>
+                <p>Somos una Empresa Constructora e Inmobiliaria de alto rendimiento, conformada por profesionales de amplia experiencia y sobre todo grandes resultados. En nuestros 20 años hemos participado en más de 20 proyectos inmobiliarios de construcción y comercialización que han resultado de éxito en su totalidad. </p>
+                <p>Nuestro objetivo es atender y resolver las necesidades de vivienda, ofreciendo a nuestros clientes inmuebles estratégicamente ubicados, de altos estándares, bien construidos, con valores agregados y a precios competitivos, siempre pensando en ofrecer inversiones seguras.</p>
+            </section>
+            <MisionVision path="quienes-somos" />
+            <section className="valores">
+                <h2>VALORES CORPORATIVOS</h2>
+                <p>nova constructores esta conformada por profesionales que brindan a cada cliente un trato único que le permite tener una experiencia única. En nuestros más de 20 de años de experiencia nuestros valores nos han permitido lograr proveer más de 2000 hogares esto gracias al profesionalismo y dedicación del equipo de trabajo que se ha conformado para al ejecución de cada proyecto</p>
+            </section>
+            <section className="sensaciones">
+                <video muted loop id="inova-sensaciones">
+                    <source src={inovaSensaciones} type="video/mp4" />
+                    <source src={inovaSensaciones} type="video/ogg" />
+                    Tu navegador no soporta la etiqueta de video.
+                </video>
+            </section>
+            {/* <section className="sand">
                 <img src={logo3} className="logo" alt="logo" />
+            </section> */}
+            <section className="row" id="row-correction">
+                <div className="col-lg-6 quienes-info">
+                    <h2>Experiencias</h2>
+                    <p>Nuestra experiencia nos ha permitido lograr proveer más de 2000 hogares esto gracias al profesionalismo y dedicación del equipo de trabajo que se ha conformado para al ejecución de cada proyecto.</p>
+
+                    <h2>Profesionalismo</h2>
+                    <p>Nuestro equipo de trabajo es de profesionales que brindan a cada cliente un trato único que le permite tener una experiencia única. En nuestros más de 20 de años de experiencia nuestros valores nos han permitido lograr proveer más de 2000 hogares esto gracias al profesionalismo y dedicación del equipo de trabajo que se ha conformado para al ejecución de cada proyecto.</p>
+
+                    <h2>Compromiso</h2>
+                    <p>Nuestro equipo de trabajo es de profesionales que brindan a cada cliente un trato único que le permite tener una experiencia única. En nuestros más de 20 de años de experiencia nuestros valores nos han permitido lograr proveer más de 2000 hogares esto gracias al profesionalismo y dedicación del equipo de trabajo que se ha conformado para al ejecución de cada proyecto.</p>
+
+                    <h2>Inmobiliarias</h2>
+                    <p>Nuestro equipo de trabajo es de profesionales que brindan a cada cliente un trato único que le permite tener una experiencia única. En nuestros más de 20 de años de experiencia nuestros valores nos han permitido lograr proveer más de 2000 hogares esto gracias al profesionalismo y dedicación del equipo de trabajo que se ha conformado para al ejecución de cada proyecto.</p>
+                </div>
+                <div className="col-lg-6 quienes-img">
+                    <img src={quienes} alt="quienes somos" />
+                </div>
             </section>
             <section className="somos">
                 <div className="row">
